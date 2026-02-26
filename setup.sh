d6363d135b134c2b6c1c4970ccc28722a69e3519
Eloy De Jong <edjong@isomorphiclabs.com>
2026-02-26 11:04:23 +0000

    Remove gemini-cli installation

--- setup.sh
+++ setup.sh
@@ -30,26 +30,6 @@ sudo apt update
 # install less
 sudo apt install less -y 
 
-# install NPM and gemini
-if [ ! -d "$HOME/.nvm" ]; then
-    curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
-fi
-
-# Load nvm into the current shell session
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
-
-nvm install --lts
-nvm use --lts
-
-# Configure global npm packages to be stored in /persistence
-mkdir -p /persistence/.npm-global
-npm config set prefix '/persistence/.npm-global'
-export PATH="/persistence/.npm-global/bin:$PATH"
-
-npm install -g @google/gemini-cli
-
 # install zsh and Oh My Zsh
 sudo apt install zsh -y
 
